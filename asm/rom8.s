c15dc1ff9dece80f51f42da99db7247c8d397ec8
camthesaxman <cameronghall@cox.net>
2017-09-16 22:34:21 -0500

    dump interface graphics

--- asm/rom8.s
+++ asm/rom8.s
@@ -6,4 +6,34 @@
 gGfx_link4bpp:
 	.INCBIN "graphics/lttp/link.4bpp"
 
-	.INCBIN "baserom.gba", 0x23482C, 0x641608-0x23482C
+	.INCBIN "baserom.gba", 0x23482C, 0x3B5758-0x23482C
+
+	.GLOBAL gUnknown_083B5758
+gUnknown_083B5758:
+	.INCBIN "graphics/lttp/hud.4bpp"
+
+	.GLOBAL gUnknown_083B6358
+gUnknown_083B6358:
+	.INCBIN "graphics/lttp/font1.4bpp"
+	
+	.GLOBAL gUnknown_083BA758
+gUnknown_083BA758:
+	.INCBIN "graphics/lttp/font2.4bpp"
+	
+	.GLOBAL gUnknown_083BB758
+gUnknown_083BB758:
+	.INCBIN "graphics/lttp/font3.4bpp"
+	
+	.GLOBAL gUnknown_083BBF58
+gUnknown_083BBF58:
+	.INCBIN "graphics/lttp/font4.4bpp"
+	
+	.GLOBAL gUnknown_083BC758
+gUnknown_083BC758:
+	.INCBIN "graphics/lttp/font5.4bpp"
+	
+	.GLOBAL gUnknown_083BCF58
+gUnknown_083BCF58:
+	.INCBIN "graphics/lttp/font6.4bpp"
+
+	.INCBIN "baserom.gba", 0x3BD758, 0x641608-0x3BD758
--- gfxdep.mk
+++ gfxdep.mk
@@ -1,3 +1,11 @@
 # This file contains graphics dependencies for each file
 
-asm/rom8.o: graphics/lttp/link.4bpp
+asm/rom8.o: \
+	graphics/lttp/link.4bpp \
+	graphics/lttp/hud.4bpp \
+	graphics/lttp/font1.4bpp \
+	graphics/lttp/font2.4bpp \
+	graphics/lttp/font3.4bpp \
+	graphics/lttp/font4.4bpp \
+	graphics/lttp/font5.4bpp \
+	graphics/lttp/font6.4bpp
